2b7af4d1b5132ff39e1cb2f43abb4128422f99f7
obroybass <rbass@outbrain.com>
2018-10-28 08:39:19 +0200

    Refactor - Move ActionButton to it's own component file

--- src/generator/action-button.js
+++ src/generator/action-button.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import { Button } from 'react-admin';
+import React, { Component, Fragment } from 'react';
+import { Button, Confirm, showNotification } from 'react-admin';
 import * as icons from '@material-ui/icons';
+import mustache from 'mustache';
 import endpointRunner from '../api/endpoint-runner';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const ActionButton = ({
-                        record = {},
-                        endpoint,
-                        icon,
-                        title,
-                        ...rest
-                      }) => (
-  <Button onClick={() => {
-    if (endpoint) {
-      endpointRunner.run(endpoint, record);
+class ActionButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  handleClick = () => {
+    if (this.props.action.confirm) {
+      this.setState({ isOpen: true });
+    } else {
+      this.runAction();
     }
-  }}>
-    {React.createElement(icons[icon])} {title}
-  </Button>
-);
+  };
 
-function generateActionButton(actionDef) {
-  return (<ActionButton {...actionDef} />);
+  handleDialogClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+
+  runAction = () => {
+    const { showNotification } = this.props;
+    const { endpoint } = this.props.action;
+    if (endpoint) {
+      endpointRunner.run(endpoint, { ids: this.props.ids })
+        .then((response) => {
+          response.text().then(text => {
+            showNotification(response.status + ' ' + response.statusText + ' - ' + text);
+          });
+        });
+    }
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    const { title, icon, confirm } = this.props.action;
+    return (
+      <Fragment>
+
+        <Button label={title} onClick={this.handleClick}>
+          {React.createElement(icons[icon])}
+        </Button>
+
+        <Confirm
+          isOpen={this.state.isOpen}
+          title={"Confirm " + title}
+          content={mustache.render(confirm, this.props)}
+          onConfirm={this.runAction}
+          onClose={this.handleDialogClose}
+        />
+      </Fragment>
+
+    );
+  }
 }
 
-export default generateActionButton;
+ActionButton.propTypes = {
+  showNotification: PropTypes.func
+};
+
+export default connect(null, { showNotification })(ActionButton);
--- src/generator/bulk-action-buttons.js
+++ src/generator/bulk-action-buttons.js
@@ -1,80 +1,14 @@
 import React, { Component, Fragment } from 'react';
-import { BulkDeleteButton, Button, Confirm, showNotification } from 'react-admin';
-import * as icons from '@material-ui/icons';
-import mustache from 'mustache';
-import endpointRunner from '../api/endpoint-runner';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { BulkDeleteButton } from 'react-admin';
+import ActionButton from './action-button';
 
-class ActionButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  handleClick = () => {
-    if (this.props.action.confirm) {
-      this.setState({ isOpen: true });
-    } else {
-      this.runAction();
-    }
-  };
-
-  handleDialogClose = () => {
-    this.setState({ isOpen: false });
-  };
-
-
-  runAction = () => {
-    const { showNotification } = this.props;
-    const { endpoint } = this.props.action;
-    if (endpoint) {
-      endpointRunner.run(endpoint, { ids: this.props.ids })
-        .then((response) => {
-          response.text().then(text => {
-            showNotification(response.status + ' ' + response.statusText + ' - ' + text);
-          });
-        });
-    }
-    this.setState({ isOpen: false });
-  };
-
-  render() {
-    const { title, icon, confirm } = this.props.action;
-    return (
-      <Fragment>
-
-        <Button label={title} onClick={this.handleClick}>
-          {React.createElement(icons[icon])}
-        </Button>
-
-        <Confirm
-          isOpen={this.state.isOpen}
-          title={"Confirm " + title}
-          content={mustache.render(confirm, this.props)}
-          onConfirm={this.runAction}
-          onClose={this.handleDialogClose}
-        />
-      </Fragment>
-
-    );
-  }
-}
-
-ActionButton.propTypes = {
-  showNotification: PropTypes.func
-};
-
-const ActionButtonWithNotifications = connect(null, { showNotification })(ActionButton);
 class BulkActionButtons extends Component {
 
   render() {
     console.log('props=', this.props);
     return (
       <Fragment>
-        {this.props.actions.map((actionDef) => React.createElement(ActionButtonWithNotifications, {
+        {this.props.actions.map((actionDef) => React.createElement(ActionButton, {
           action: actionDef,
           ids: this.props.selectedIds,
           key: "_action_" + actionDef.title
